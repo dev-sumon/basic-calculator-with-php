5b4c853bfbdfc6d13c9d538682f51cca0efa7e9e
dev-sumon <146063221+dev-sumon@users.noreply.github.com>
2024-01-28 19:49:36 +0600

    desigen part added

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        <?php
+            include"style.css"
+        ?>
+    </style>
+</head>
+<body>
+    <div class="container">
+    <h2>Basic Calculator</h2>
+    <form method="post" action="">
+        <input type="number" name="num1" id="">
+        <input type="number" name="num2" id="">
+        <select name="" id="">
+            <option value="addition">Addition</option>
+            <option value="subtraction">Subtraction</option>
+            <option value="multiplication">Multiplication</option>
+            <option value="division">Division</option>
+        </select><br>
+        <button>Calculate</button>
+    </form>
+    <div id="result">
+        <?php
+        if()
+        
+        ?>
+
+    </div>
+    </div>
+</body>
+</html>
